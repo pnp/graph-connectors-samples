19ff7884d904b0a535b02c7b44f7d6eab00b3aa0
Dan Toft <tanddant@gmail.com>
2023-10-27 09:47:37 +0200

    🎉 - Added PowerShell setup script

---
+++ samples/nodejs-javascript-solutiongallery/setup.ps1
@@ -0,0 +1,13 @@
+# login
+Write-Host "Sign in to Microsoft 365..."
+npx -p @pnp/cli-microsoft365 -c 'm365 login'
+
+# create AAD app
+Write-Host "Creating AAD app..."
+$appInfo=$(npx -p @pnp/cli-microsoft365 -- m365 aad app add --name "Sample Solution Gallery - connector" --withSecret --apisApplication "https://graph.microsoft.com/ExternalConnection.ReadWrite.OwnedBy, https://graph.microsoft.com/ExternalItem.ReadWrite.OwnedBy" --grantAdminConsent --output json)
+
+# write app to env.js
+Write-Host "Writing app to env.js..."
+New-Item -ItemType File -Name "env.js" -Value "export const appInfo = $($appInfo | Out-String)" -Force
+
+Write-Host "DONE"
